ab6a25859ba915797670a6a2af25887f44dfb8ba
Ederson <edersonvidal137@gmail.com>
2025-08-18 21:22:46 -0300

    Project Created
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/br/dev/ederson/intensivao/sprintboot/dslistmine/DslistMineApplication.java
@@ -0,0 +1,13 @@
+package br.dev.ederson.intensivao.sprintboot.dslistmine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistMineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DslistMineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/dev/ederson/intensivao/sprintboot/dslistmine/DslistMineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.ederson.intensivao.sprintboot.dslistmine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistMineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
